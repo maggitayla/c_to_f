2557aa6b04dfb4adedac0557ecdfadfe0c57dfea
Taylor Hsu <45rtfgvb123456@gmail.com>
2021-03-14 14:35:26 +0800

    add bmi practice

--- hi.py
+++ hi.py
@@ -4,7 +4,23 @@
 #eg.git commit -m "my file name is 123.py"
 #3.git push origin main
 
-c = input('請輸入攝氏溫度: ')
-c = float(c)
-f = c * 9 / 5 + 32
-print('華氏溫度為: ', f)
+# c = input('請輸入攝氏溫度: ')
+# c = float(c)
+# f = c * 9 / 5 + 32
+# print('華氏溫度為: ', f)
+
+h = int(input('anwser your height: '))
+w = int(input('anwser your weight: '))
+bmi = w / h**2
+if bmi < 18.5:
+    print('過輕')
+elif bmi >= 18.5 and bmi <24:
+    print('正常')
+elif bmi >= 24 and bmi < 27:
+    print('過重')
+elif bmi >= 27 and bmi < 30:
+    print('輕度肥')
+elif bmi >= 30 and bmi < 35:
+    print('中度肥')
+else:
+    print('重度肥')
